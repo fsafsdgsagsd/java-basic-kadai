ebbfcc68f3374496ba840d45f5592f6fc27ef55e
fsafsdgsagsd <148539543+fsafsdgsagsd@users.noreply.github.com>
2024-04-20 16:22:21 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,21 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+	public static void main(String[] args) {
+		  KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+		  KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		  KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		      
+		  taro.setGivenName("");
+		  taro.commonIntroduce();
+		  taro.execIntroduce();
+		  
+		  ichiro.setGivenName("");
+		  ichiro.commonIntroduce();
+		  ichiro.execIntroduce();
+		  
+		  hanako.setGivenName("");
+		  hanako.commonIntroduce();
+		  hanako.execIntroduce();
+	 }
+}
